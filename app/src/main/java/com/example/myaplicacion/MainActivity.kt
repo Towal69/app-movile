368f41b7c98721dc5b0c3c7f1b9a5ea29aa13d86
Sebastian Castillo <tantrum5535@gmail.com>
2024-08-30 20:19:12 -0400

    Configuracion inicial

--- app/src/main/java/com/example/myaplicacion/MainActivity.kt
+++ app/src/main/java/com/example/myaplicacion/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: MainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding =MainActivityBinding.inflate(layoutInflater)
+        binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
