1bc43b0fb582d6e249b69b5101c2f3911fd8c38d
Ana Céspedes <anacespedes@MacBook-Pro-de-Ana.local>
2024-09-28 20:21:05 -0300

    limpiando el proyecto

--- app/src/main/java/com/example/myaplicacion/view/bienvenido/BienvenidoFragment.kt
+++ app/src/main/java/com/example/myaplicacion/view/bienvenido/BienvenidoFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -42,63 +41,27 @@ class BienvenidoFragment : Fragment() {
             db.collection("users").document(it)
                 .get()
                 .addOnSuccessListener { document ->
-                    if(document != null){
+                    if (document != null) {
                         val nombre = document.getString("nombre")
                         binding.topAppBar.title = "$nombre"
-                    }else{
+                    } else {
                         Toast.makeText(context, "Error al obtener los datos del usuario", Toast.LENGTH_SHORT).show()
                     }
                 }
         }
-        binding.topAppBar.setNavigationOnClickListener {
-            showPopupMenu(it)
-        }
+
         // Configurar el botón de cierre de sesión
         binding.topAppBar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
-
-                R.id.search -> {
-                    Toast.makeText(context, "Search clicked", Toast.LENGTH_SHORT).show()
-                    true
-                }
                 R.id.more -> {
-                    // Cerrar sesión y navegar al fragmento de inicio de sesión
                     auth.signOut()
                     findNavController().navigate(R.id.loginFragment3)
                     true
                 }
-                R.id.menu -> {
-                    showPopupMenu(binding.topAppBar)
-                    true
-                }
                 else -> false
             }
         }
     }
-    private fun showPopupMenu(view: View) {
-        val popupMenu = PopupMenu(requireContext(), view)
-        popupMenu.inflate(R.menu.popup_menu) // Inflar el menú de categorías
-        popupMenu.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.categoria1 -> {
-                    Toast.makeText(context, "Categoría 1 seleccionada", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.categoria2 -> {
-                    Toast.makeText(context, "Categoría 2 seleccionada", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.categoria3 -> {
-                    Toast.makeText(context, "Categoría 3 seleccionada", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                else -> false
-            }
-        }
-        popupMenu.show()
-    }
-
-
 
     override fun onDestroyView() {
         super.onDestroyView()
